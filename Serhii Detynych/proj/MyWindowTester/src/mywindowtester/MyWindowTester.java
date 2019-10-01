e153d37ff791a439a10f444ab053deae60144310
azureitstepkharkov <mikelietko@gmail.com>
2019-10-01 21:29:57 +0300

    MyWindow project

---
+++ Serhii Detynych/proj/MyWindowTester/src/mywindowtester/MyWindowTester.java
@@ -0,0 +1,11 @@
+package mywindowtester;
+
+public class MyWindowTester {
+
+    public static void main(String[] args)
+    {
+        MyWindow w = new MyWindow();
+        w.setVisible(true);
+    }
+    
+}
