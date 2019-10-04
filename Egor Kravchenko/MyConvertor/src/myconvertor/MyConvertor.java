081e07a46ebc9be8385eb364a04b8367d3b26446
Egor <brazuca.egor@gmail.com>
2019-10-04 14:56:53 +0300

    add convertor dz pyatnitsa

---
+++ Egor Kravchenko/MyConvertor/src/myconvertor/MyConvertor.java
@@ -0,0 +1,13 @@
+
+package myconvertor;
+
+
+public class MyConvertor {
+
+ 
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.setVisible(true);
+    }
+    
+}
