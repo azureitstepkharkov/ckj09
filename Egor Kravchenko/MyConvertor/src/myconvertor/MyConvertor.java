9702aae53497824da743959465c9a6db1cb7eb43
Egor <brazuca.egor@gmail.com>
2019-10-07 10:41:51 +0300

    полный конвертер

--- Egor Kravchenko/MyConvertor/src/myconvertor/MyConvertor.java
+++ Egor Kravchenko/MyConvertor/src/myconvertor/MyConvertor.java
@@ -20,25 +20,25 @@ public class MyConvertor {
         
         Panell grRad = new Panell();
         JPanel jp2 = new JPanel();
-        jp2 = kmToMl.creat("градусы","радианы","convert",2);
+        jp2 = grRad.creat("градусы","радианы","convert",2);
         window.add(jp2);
         jp1.setLayout(new FlowLayout());
         
         Panell grFar = new Panell();
         JPanel jp3 = new JPanel();
-        jp3 = kmToMl.creat("градусы","фаренгейты","convert",3);
+        jp3 = grFar.creat("градусы","фаренгейты","convert",3);
         window.add(jp3);
         jp3.setLayout(new FlowLayout());
         
         Panell grKel = new Panell();
         JPanel jp4 = new JPanel();
-        jp4 = kmToMl.creat("градусы","Кельвины","convert",4);
+        jp4 = grKel.creat("градусы","Кельвины","convert",4);
         window.add(jp4);
         jp4.setLayout(new FlowLayout());
          
         Panell farKel = new Panell();
         JPanel jp5 = new JPanel();
-        jp5 = kmToMl.creat("Фаренгейты","Кельвины","convert",5);
+        jp5 = farKel.creat("Фаренгейты","Кельвины","convert",5);
         window.add(jp5);
         jp5.setLayout(new FlowLayout());
         
