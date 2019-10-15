b388a600a42cc5d08eefb45cfebb089b43c55c92
Anatolii Ageev <anatol1812@gmail.com>
2019-10-15 20:39:31 +0300

    Added project jdbc connection

--- Anatolii Ageev/MyConverter/src/com/ageev/MyConverter.java
+++ Anatolii Ageev/MyConverter/src/com/ageev/MyConverter.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 
 public class MyConverter {
     public static void main(String[] args) {
-        // MyWindow w = new MyWindow();
-        // w.setVisible(true);
+       //  MyWindow w = new MyWindow();
+       //  w.setVisible(true);
         // GroupLayoutTest glt = new GroupLayoutTest();
         // glt.setVisible(true);
 
@@ -23,8 +23,17 @@ public class MyConverter {
 //                new GroupLayoutTest().setVisible(true);
 //            }
 //        });
+         JLabel l = new JLabel("Kmss: ");
+         JLabel m = new JLabel("mlss");
+         JButton c = new JButton("sss->");
+        JTextField f1  = new JTextField();
+        JTextField f2  = new JTextField();
 
-        new GroupLayoutTest().setVisible(true);
+        GroupLayoutTest t1 = new GroupLayoutTest(l, m, c, f1, f2);
+            t1.setVisible(true);
+        GroupLayoutTest t2 =   new GroupLayoutTest(l, m, c, f1, f2);
+            t2.setVisible(true);
+
     }
 }
 
