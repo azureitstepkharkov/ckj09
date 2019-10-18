0976bc5c539ea76d74e9b7e863a1f7ec19fee208
Anatolii Ageev <anatol1812@gmail.com>
2019-10-18 21:30:53 +0300

    Classwork added

---
+++ Anatolii Ageev/ComboBoxTest/src/model/Departments.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.Objects;
+
+public class Departments {
+    int id;
+    String name;
+
+    public Departments(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Departments{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departments that = (Departments) o;
+        return id == that.id &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
