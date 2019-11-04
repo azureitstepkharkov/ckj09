9e2fe1d61b4423b0c50c148799e8b8c80d29994a
azureitstepkharkov <azure.itstep.kharkov@gmail.com>
2019-11-04 20:42:30 +0200

    расширенный пример рефлексии

--- Ivan Petrov/JavaReflectionTest/src/javareflectiontest/JavaReflectionTest.java
+++ Ivan Petrov/JavaReflectionTest/src/javareflectiontest/JavaReflectionTest.java
@@ -27,7 +27,12 @@ public class JavaReflectionTest {
             // те же шаги
             method = aUser.getClass().getMethod("getEmail");
             userEmail = (String) method.invoke(aUser);
-                } catch (NoSuchMethodException ex) {
+            //
+            //вызов метода, которому нужен параметр
+            method = aUser.getClass().getMethod("setEmail", String.class);
+            method.invoke(aUser, "tester2@gmail.com");
+            //
+        } catch (NoSuchMethodException ex) {
             System.out.println("в классе нет метода с таким именем");;
         } catch (SecurityException ex) {
             System.out.printf("Нет прав администратора для приложения. %s !", ex.toString());
@@ -38,7 +43,61 @@ public class JavaReflectionTest {
         } catch (InvocationTargetException ex) {
             System.out.println("Неизвестная А.К. ошибка");
         }
-         
+        //
+        //Список методов класса
+        String className = "javareflectiontest.MyUser";
+        System.out.println("Список методов класса:" + className);
+        //Class clazz = MyUser.class;
+        Class clazz;
+        try {
+            clazz = Class.forName(className);
+            System.out.println("у класса есть аннотация "+clazz.isAnnotation()); 
+            Class superClasses =  clazz.getSuperclass();
+            System.out.println("у класса есть родитель "+superClasses); 
+            Method[] methods = clazz.getMethods();
+            //в дополнение ко всем "моим" открытм методам
+           //идут все унаследованные от System.Object
+           //и от родителей + "готовность" рботать с потоками:wait notify ...
+           System.out.println("методы класса ");
+            for (Method function : methods) {
+                System.out.println(function.getName());
+                //
+                 if (  function.getParameterCount() == 1//если у метода только один параметр
+                    && function.getParameterTypes()[0].//и тип этого параметра String
+                       getName().indexOf("String") >= 0)
+                 {
+                       function.invoke(aUser, "testvalue");
+                 }
+                //
+            }
+            //import java.lang.reflect.Constructor;
+            Constructor[] contrs = clazz.getConstructors();
+            System.out.println("конструкторы: ");
+            for (Constructor function : contrs) {
+                System.out.println(function.getName()+" параметры "+function.getParameterCount() );
+                
+            }
+            //
+            Field[]  fields =  clazz.getFields();
+            Class[] interfaces = clazz.getInterfaces();
+            System.out.println("intefaces : ");
+            for(Class c : interfaces)
+            {
+                System.out.println(c);
+            }
+            //
+        } 
+        catch (ClassNotFoundException ex) {
+            Logger.getLogger(JavaReflectionTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(JavaReflectionTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(JavaReflectionTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(JavaReflectionTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+       
         
     }
     
--- Ivan Petrov/JavaReflectionTest/src/javareflectiontest/MyUser.java
+++ Ivan Petrov/JavaReflectionTest/src/javareflectiontest/MyUser.java
@@ -1,6 +1,8 @@
 package javareflectiontest;
-
-public class MyUser 
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.*;
+public class MyUser implements Action
 {
 
     public MyUser(Long id, String email, String password) {
@@ -10,7 +12,6 @@ public class MyUser
         System.out.println("call MyUser(Long id...");
     
     }
-
     public MyUser() 
     {
        System.out.println("call MyUser()");
@@ -54,5 +55,40 @@ public class MyUser
     private Long id;
     private String email;
     private String password;
+
+    @Override
+    public Object getValue(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void putValue(String string, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEnabled(boolean bln) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEnabled() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener pl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener pl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
