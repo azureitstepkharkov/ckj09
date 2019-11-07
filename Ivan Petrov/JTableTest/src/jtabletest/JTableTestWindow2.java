8bd816775371c0f3e75f840014473382e1762eb6
azureitstepkharkov <azure.itstep.kharkov@gmail.com>
2019-11-07 20:46:20 +0200

    JTableTest часть 2

---
+++ Ivan Petrov/JTableTest/src/jtabletest/JTableTestWindow2.java
@@ -0,0 +1,30 @@
+package jtabletest;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
+class JTableTestWindow2 extends JFrame 
+{
+    JTable tblString;
+    public JTableTestWindow2()
+    {
+        super("JTable test 2");
+        setSize(200, 400);
+        //матрица значений
+        List<Student> students = new ArrayList();
+        students.add(new Student(1,"Иванов",Student.Grade.A));
+        students.add(new Student(2,"Петров",Student.Grade.B));
+        students.add(new Student(3,"Сидоров",Student.Grade.C));
+        StudentTableModel model = new StudentTableModel(students);
+        tblString = new JTable(model);
+        setLayout( new BorderLayout() );
+        add( new JScrollPane( tblString) ,BorderLayout.CENTER);
+        //
+        //tblString.setDefaultRenderer(Student.Grade.class, new MyGradeRenderer());
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
---
+++ Ivan Petrov/JTableTest/src/jtabletest/Student.java
@@ -0,0 +1,88 @@
+package jtabletest;
+
+import java.util.Objects;
+
+public class Student {
+    
+    public static enum Grade
+    {
+        A,B,C;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.grade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.grade != other.grade) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", grade=" + grade + '}';
+    }
+
+    public Student(int id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Student() {
+    }
+    
+    private int id;
+    private String name;
+    private Grade grade;
+    //setters,getters, and other
+}
+
